d9b4aad55de8d7cd193495f763e042232c4a6529
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-10-18 23:10:23 +0200

    add animate

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,12 +1,19 @@
 class Cloud extends MovableObject{
     y = 20;
     width  = 500;
-    height = 300;	
+    height = 300;
 
     constructor(){
         super().loadImage('../img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500;
         
+        this.animate();
     }
+
+    animate(){
+        this.moveLeft();
+    }
+
+    
 }
