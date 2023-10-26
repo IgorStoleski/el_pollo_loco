745184ecae8ced953b41403dd740a363cdba7063
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-10-26 12:23:39 +0200

    set x position

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -19,7 +19,7 @@ class Endboss extends MovableObject {
     constructor() {
         super().loadImage('img/4_enemie_boss_chicken/2_alert/G5.png');
         this.loadImages(this.IMAGES_WALKING);
-        this.x = 700;
+        this.x = 2500;
         this.animate();
     }
 
