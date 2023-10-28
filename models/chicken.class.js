d699584f263dad04795c5eb31a22a3f52b9446af
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-10-28 08:59:00 +0200

    some tweaks

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -7,7 +7,7 @@ class Chicken extends MovableObject{
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
-    ]
+    ];
     walking_chicken_sound = new Audio('audio/chicken.mp3');
     
 
@@ -16,7 +16,7 @@ class Chicken extends MovableObject{
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
 
-        this.x = 200 + Math.random() * 500;
+        this.x = 500 + Math.random() * 2000;
         this.speed = 0.2 + Math.random() * 0.3;
         //this.animate();
     }
