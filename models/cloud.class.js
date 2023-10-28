2b04e3d7ba4dbb1b11dbaf35a3b723564e3347e3
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-10-28 08:59:13 +0200

    add more clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -3,12 +3,30 @@ class Cloud extends MovableObject{
     width  = 500;
     height = 300;
 
-    constructor(){
+    constructor(zone){
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
-        this.x = Math.random() * 500;
+        this.x = this.generateRandomXPositionForZone(zone);
         
         this.animate();
+    }
+
+    generateRandomXPositionForZone(zone){
+        let minX, maxX;
+
+        // Abhängig von der Zone, setze die Grenzen für die X-Position
+        if(zone === 1){
+            minX = 0;
+            maxX = 719;
+        } else if(zone === 2){
+            minX = 720;
+            maxX = 1439;
+        } else if(zone === 3){
+            minX = 1440;
+            maxX = 2159;
+        }
+
+        return Math.random() * (maxX - minX) + minX;
     }
 
     animate(){
