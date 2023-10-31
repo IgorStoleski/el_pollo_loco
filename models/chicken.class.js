b39effe0f320411c8e2b00bd25c328204720ac2f
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-11-01 00:24:43 +0100

    add image_dead

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -8,6 +8,12 @@ class Chicken extends MovableObject{
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
+
+    IMAGES_DEAD = [
+        'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
+    ];
+
+
     walking_chicken_sound = new Audio('audio/chicken.mp3');
     
 
@@ -15,7 +21,7 @@ class Chicken extends MovableObject{
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
-
+        this.loadImage(this.IMAGES_DEAD);
         this.x = 500 + Math.random() * 2000;
         this.speed = 0.2 + Math.random() * 0.3;
         //this.animate();
