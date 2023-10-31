2065c5c20b29526f45bf8047506e24211bea64cd
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-11-01 00:26:19 +0100

    add some function

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,12 +1,29 @@
 class ThrowableObject extends MovableObject {
 
+    THROW_IMAGES = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ];
+
+    SPLASH_IMAGES = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
+    ];
 
     constructor(x, y) {
-        super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        super().loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
+        this.loadImages(this.THROW_IMAGES);
+        this.loadImages(this.SPLASH_IMAGES);
         this.x = x;
         this.y = y;
-        this.height = 60;
-        this.width = 50;
+        this.height = 100;
+        this.width = 100;
         this.thow(x, y);
         
     }
@@ -16,9 +33,16 @@ class ThrowableObject extends MovableObject {
         this.x = x;
         this.y = y;
         this.speedY = 30;
-        this.applyGravity();
+        this.applyGravity();        
         setInterval(() => {
+            this.playAnimation(this.THROW_IMAGES);
             this.x += 10;
         }, 25);
+    }
+
+    splashBottle() {
+        if (this.bottle.isColliding(this.world.level.enemies.Endboss)) {
+            this.playAnimation(this.SPLASH_IMAGES);
+        }
     }
 }
