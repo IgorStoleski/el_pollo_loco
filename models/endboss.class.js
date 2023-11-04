800ad1085a9068279f926f4af4a7f577ad31b91c
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-11-04 23:10:19 +0100

    add images and logic

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,5 +1,4 @@
 class Endboss extends MovableObject {
-
     height = 400;
     width = 250;
     y = 45;
@@ -15,17 +14,44 @@ class Endboss extends MovableObject {
         'img/4_enemie_boss_chicken/2_alert/G12.png'
     ];
 
+    IMAGES_HURT = [
+        'img/4_enemie_boss_chicken/4_hurt/G21.png',
+        'img/4_enemie_boss_chicken/4_hurt/G22.png',
+        'img/4_enemie_boss_chicken/4_hurt/G23.png'
+    ];
+
+    IMAGES_DEAD = [
+        'img/4_enemie_boss_chicken/5_dead/G24.png',
+        'img/4_enemie_boss_chicken/5_dead/G25.png',
+        'img/4_enemie_boss_chicken/5_dead/G26.png'
+    ];
+
 
     constructor() {
         super().loadImage('img/4_enemie_boss_chicken/2_alert/G5.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 2500;
         this.animate();
     }
 
+
     animate() {
-        setInterval(() => {
+        /* setInterval(() => {
+            
             this.playAnimation(this.IMAGES_WALKING);
+            
+        }, 200); */
+
+        setInterval(() => {
+            if (this.isHurt()) {
+                this.playAnimation(this.IMAGES_HURT);
+            } else if (this.bossIsDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+            } else {
+                this.playAnimation(this.IMAGES_WALKING);
+            }
         }, 200);
     }
 
