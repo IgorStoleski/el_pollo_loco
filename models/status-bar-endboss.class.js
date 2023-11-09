d27768ca25564c9b7b77634f0b1ce780d42d2285
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-11-09 23:30:57 +0100

    add statusbar

---
+++ models/status-bar-endboss.class.js
@@ -0,0 +1,51 @@
+class StatusBarEndboss extends DrawableObject {
+    
+
+    IMAGES_HEALTH = [
+        'img/7_statusbars/1_statusbar/2_statusbar_health/orange/0.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/orange/40.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png'
+    ];
+
+    IMAGE_BOSS_HEALTH_ICON = [
+        'img/7_statusbars/3_icons/icon_health_endboss.png'
+    ];
+
+    percentage = 100;
+
+    constructor() {
+        super();
+        this.loadImages(this.IMAGES_HEALTH);
+        this.x = 500;
+        this.y = 0;
+        this.width = 200;
+        this.height = 60;
+        this.setPercentage(100);
+    }
+
+    setPercentage(percentage) {
+        if (percentage >= 0) {
+            this.percentage = percentage;
+            let path = this.IMAGES_HEALTH[this.resolveImageIndex()];
+            this.img = this.imageCache[path];
+        }
+    }
+
+    resolveImageIndex() {
+        if (this.percentage == 100 ) {
+            return 4;
+        } else if (this.percentage == 75) {
+            return 3;
+        } else if (this.percentage == 50) {
+            return 2;
+        } else if (this.percentage == 25) {
+            return 1;
+        } else if (this.percentage == 0) {
+            return 0;
+        } 
+    }
+
+    
+}
