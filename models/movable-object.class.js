4e7ade8b9b94171078a80d957833341071764c55
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-11-11 22:27:13 +0100

    update isColliding

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -7,6 +7,13 @@ class MovableObject extends DrawableObject {
     bossEnergy = 100;
     lastHit = 0;
 
+    offset = { 
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+    };
+
 
     applyGravity(){
         setInterval(() =>{
@@ -25,11 +32,11 @@ class MovableObject extends DrawableObject {
         }
     }
 
-    isColliding(mo){
-        return this.x + this.width > mo.x
-            && this.y + this.height > mo.y
-            && this.x < mo.x
-            && this.y < mo.y + mo.height;
+    isColliding (mo) {
+        return  this.x + this.width - this.offset.right > mo.x + mo.offset.left &&
+                this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&
+                this.x + this.offset.left < mo.x + mo.width - mo.offset.right &&
+                this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom;
     }
 
     hit() {
@@ -57,9 +64,6 @@ class MovableObject extends DrawableObject {
     bottleHitBoss() {
         if (this.bossEnergy > 0) {
             this.bossEnergy -= 25;
-            if (this.bossEnergy < 0) {
-                this.bossEnergy = 0;
-            }
             this.lastHit = new Date().getTime();
         }
     }
