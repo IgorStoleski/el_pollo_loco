61f4e416a65ba07dc737d5b58f9e444c27894d4d
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-11-20 23:13:58 +0100

    add images

--- models/character.class.js
+++ models/character.class.js
@@ -4,6 +4,8 @@ class Character extends MovableObject{
     y = 130;
     speed = 3;
     imageCache = [];
+    timeSinceLastInput = 0;
+    timeThreshold = 2000;
 
     offset = {
         top: 120,
@@ -11,7 +13,7 @@ class Character extends MovableObject{
         right: 30,
         bottom: 10
     };
-
+    wortd;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -50,9 +52,34 @@ class Character extends MovableObject{
         'img/2_character_pepe/5_dead/D-57.png'
     ];
 
+    IMAGES_IDLE = [
+        'img/2_character_pepe/1_idle/idle/I-1.png',
+        'img/2_character_pepe/1_idle/idle/I-2.png',
+        'img/2_character_pepe/1_idle/idle/I-3.png',
+        'img/2_character_pepe/1_idle/idle/I-4.png',
+        'img/2_character_pepe/1_idle/idle/I-5.png',
+        'img/2_character_pepe/1_idle/idle/I-6.png',
+        'img/2_character_pepe/1_idle/idle/I-7.png',
+        'img/2_character_pepe/1_idle/idle/I-8.png',
+        'img/2_character_pepe/1_idle/idle/I-9.png',
+        'img/2_character_pepe/1_idle/idle/I-10.png'
+    ];
+
+    IMAGES_LONG_IDLE = [
+        'img/2_character_pepe/1_idle/long_idle/I-11.png',
+        'img/2_character_pepe/1_idle/long_idle/I-12.png',
+        'img/2_character_pepe/1_idle/long_idle/I-13.png',
+        'img/2_character_pepe/1_idle/long_idle/I-14.png',
+        'img/2_character_pepe/1_idle/long_idle/I-15.png',
+        'img/2_character_pepe/1_idle/long_idle/I-16.png',
+        'img/2_character_pepe/1_idle/long_idle/I-17.png',
+        'img/2_character_pepe/1_idle/long_idle/I-18.png',
+        'img/2_character_pepe/1_idle/long_idle/I-19.png',
+        'img/2_character_pepe/1_idle/long_idle/I-20.png',
+    ];
+
 
     world;
-    walking_sound = new Audio('audio/walking.mp3');
     
 
     constructor(){
@@ -61,42 +88,53 @@ class Character extends MovableObject{
         this.loadImages(this.IMAGES_JUMPING);
         this.loadImages(this.IMAGES_HURT);
         this.loadImages(this.IMAGES_DEAD);
-        this.jump_sound = new Audio('audio/jump.mp3');
-        this.air_sound = new Audio('audio/air.mp3');
+        this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_LONG_IDLE);
         this.applyGravity();
         this.animate();
 
     }
 
-    animate(){
+    
 
+    animate(){
         setInterval(() => {
-            this.walking_sound.pause();
+            if(!this.world.keyboard.RIGHT || !this.world.keyboard.LEFT) {
+                this.playAnimation(this.IMAGES_IDLE);
+                this.playAnimation(this.IMAGES_LONG_IDLE);
+            }
+        }, 250);
+
+        
+        setInterval(() => {
+            
+            this.world.walking_sound.pause();
             if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.moveRight();
                 this.otherDirection = false;
                 if (!this.isAboveGround()) {
-                    this.walking_sound.play();
+                    this.world.walking_sound.play();
                 } else {
-                    this.air_sound.play();
+                    this.world.air_sound.play();
                 }
             }
             if(this.world.keyboard.LEFT && this.x > 0) {
                 this.moveLeft();
                 this.otherDirection = true;
                 if (!this.isAboveGround()) {
-                    this.walking_sound.play();
+                    this.world.walking_sound.play();
                 } else {
-                    this.air_sound.play();
+                    this.world.air_sound.play();
                 }
                 
             }
 
             if(this.world.keyboard.SPACE  && !this.isAboveGround()) {
                 this.jump();
-                this.jump_sound.play();
+                this.world.jump_sound.play();
             }
-
+            
+            
             this.world.camera_x = -this.x + 100;
         }, 1000/60);
 
